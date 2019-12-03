8cb85897038792301fd56542c9693583aa08fb32
WangJinzhong <andywang7259@163.com>
2019-12-03 18:40:23 +0800

    default setting

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -34,12 +34,14 @@ module UsersHelper
   end
 
   def history_default_tab_options
-    [[l('label_issue_history_notes'), 'notes'],
-     [l('label_history'), 'history'],
-     [l('label_issue_history_properties'), 'properties'],
-     [l('label_time_entry_plural'), 'time_entries'],
-     [l('label_associated_revisions'), 'changesets'],
-     [l('label_last_tab_visited'), 'last_tab_visited']]
+    [
+      [l('label_history'), 'history'],
+      [l('label_issue_history_notes'), 'notes'],
+      [l('label_issue_history_properties'), 'properties'],
+      [l('label_time_entry_plural'), 'time_entries'],
+      [l('label_associated_revisions'), 'changesets'],
+      [l('label_last_tab_visited'), 'last_tab_visited']
+    ]
   end
 
   def change_status_link(user)
