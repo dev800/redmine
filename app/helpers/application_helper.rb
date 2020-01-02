3c21da0e99039015e47150d7560bbb4ae1fde6a1
WangJinzhong <andywang7259@163.com>
2020-01-02 11:14:49 +0800

    fix: text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1651,7 +1651,7 @@ module ApplicationHelper
           source
         end
 
-      "#{src}?v=201912271844"
+      "#{src}?v=201912301029"
     end
     super *sources, options
   end
@@ -1689,7 +1689,7 @@ module ApplicationHelper
     end
 
     sources = sources.map do |source|
-      "#{source}?v=201912271844"
+      "#{source}?v=201912301029"
     end
 
     super *sources, options
